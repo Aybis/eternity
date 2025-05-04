e77c67f31c1bfbeac370d6f03dcefc2d1d86e771
AbyanCore <abyanstudentdev@gmail.com>
2025-05-04 13:16:27 +0700

    [ADJUST] mini test transaction

--- src/components/ui/wallet/WalletModal.tsx
+++ src/components/ui/wallet/WalletModal.tsx
@@ -7,12 +7,38 @@ import {
 import React from "react";
 import "@solana/wallet-adapter-react-ui/styles.css";
 import useWeb3 from "@/hooks/useWeb3";
+// import { Transaction } from "@solana/web3.js";
 
 export default function WalletModal() {
-  const _ = useWeb3();
+  const web3 = useWeb3();
 
   return (
     <WalletModalProvider>
+      {/* <button
+        onClick={() => {
+          fetch(
+            "http://localhost:3000/api/personality/" + web3.wallet.publicKey,
+            {
+              method: "POST",
+              body: JSON.stringify({
+                name: "John Doe",
+                age: 30,
+                hobbies: ["reading", "gaming"],
+                message: "Hello, world!",
+              }),
+            }
+          ).then(async (res) => {
+            const data = await res.json();
+            await web3.wallet.sendTransaction(
+              Transaction.from(Buffer.from(data.data, "base64")),
+              web3.connection.connection
+            );
+          });
+        }}
+        className="bg-red-500 h-3"
+      >
+        ClickMe
+      </button> */}
       <WalletMultiButton />
     </WalletModalProvider>
   );
