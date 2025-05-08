cc14310f76a1275d669c55c4f9a7d877ca10f7c6
muchtar <abdul.muchtar@ai.astra.co.id>
2025-05-08 13:53:27 +0700

    [ADD] Waitlist

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
 
   return (
     <nav className="fixed top-0 w-full bg-white/30 backdrop-blur-sm bg-opacity-95 z-50  shadow-sm dark:bg-zinc-950/30 dark:border-zinc-700 ">
-      <div className="container mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 ">
+      <div className="container mx-auto max-w-6xl px-4 sm:px-6 lg:px-4 ">
         {/* Menu Content  */}
         <div className="flex justify-between items-center py-4">
           {/* Header Logo */}
@@ -40,7 +40,7 @@ export default function Header() {
 
           {/* Button Get Started */}
           <div className="hidden md:flex items-center space-x-8">
-            <div className="flex gap-x-2">
+            <div className=" gap-x-2 hidden">
               <button className="px-4 py-2 rounded-md bg-gradient-to-r from-purple-600 to-indigo-600 text-white font-medium text-sm hover:from-purple-700 hover:to-indigo-700 transition-colors">
                 Contact
               </button>
